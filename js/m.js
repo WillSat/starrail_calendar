ec566d8d4845aeedef47687d76e04e8f5562f554
WillSat <will.in@outlook.com>
2025-04-08 12:26:06 +0800

    2504081226

--- js/m.js
+++ js/m.js
@@ -2,42 +2,23 @@ const beginEle = document.getElementById('textarea_begin'),
     eventsEle = document.getElementById('textarea_events'),
     endEle = document.getElementById('textarea_end');
 
+function hashString(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = ((hash << 5) - hash) + str.charCodeAt(i);
+        hash |= 0;
+    }
+    return `${str.length.toString(16)}${hash.toString(16)}`;
+}
+
 class vEvent {
     constructor(start, end, summary, desc, rrule) {
-        this.uid = this.generateUUID();
+        this.uid = hashString(`${start}${end}${summary}${desc}${rrule}`);
         this.start = start;
         this.end = end;
         this.summary = summary;
         this.desc = desc;
         this.rrule = rrule;
-    }
-
-    generateUUID() {
-        if (typeof crypto === 'object') {
-            if (typeof crypto.randomUUID === 'function') {
-                return crypto.randomUUID();
-            }
-            if (typeof crypto.getRandomValues === 'function' && typeof Uint8Array === 'function') {
-                const callback = (c) => {
-                    const num = Number(c);
-                    return (num ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (num / 4)))).toString(16);
-                };
-                return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, callback);
-            }
-        }
-        let timestamp = new Date().getTime();
-        let perforNow = (typeof performance !== 'undefined' && performance.now && performance.now() * 1000) || 0;
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-            let random = Math.random() * 16;
-            if (timestamp > 0) {
-                random = (timestamp + random) % 16 | 0;
-                timestamp = Math.floor(timestamp / 16);
-            } else {
-                random = (perforNow + random) % 16 | 0;
-                perforNow = Math.floor(perforNow / 16);
-            }
-            return (c === 'x' ? random : (random & 0x3) | 0x8).toString(16);
-        });
     }
 
     output() {
